508db17f022fb9fff6c4e0a15a1f1b990e3e8422
Dan Finnegan <dfinny110@aol.com>
2015-04-18 20:21:59 -0400

    these are updated files for the koopa class of enemy hierarchy

---
+++ koopa.cpp
@@ -0,0 +1,54 @@
+#include <SDL2/SDL.h>
+#include <SDL/SDL_image.h>
+#include "Ltexture.h"
+#include "enemy.h"
+#include "globalVars.h"
+#include "koopa.h"
+using namespace std;
+
+Koopa::Koopa(int num, int x, int y, int w, int h, int offset, int startX, int startY): Enemy(num, x, y, w, h, offset, startX, startY){
+	//koopas are basic too, nothing else special to them
+}
+
+void Koopa::move(SDL_Rect *camera){
+   //move enemy to the left or right
+   mPosX += mVelX;
+
+   //object can't go back in map
+
+   //if object reaches the end of the screen then change direction
+   //this set up will be used for object collision
+   if( (mPosX + ENEMY_WIDTH > SCREEN_WIDTH) || (mPosX < 0) ){
+      mVelX *=(-1);
+   }
+
+   frame++;
+   decideFrame();
+}
+
+void  Koopa::decideFrame(){
+   /*// if moving left
+   if(mVelX < 0){
+      if(frame == 0)
+         frame = 1;
+      if(frame == 1)
+         frame = 0;
+   }if(mVelX >= 0){  
+   // if moving right
+      if(frame == 2)
+         frame = 3;
+      if(frame == 3)
+         frame = 2;
+   }*/
+   if(mVelX < 0){
+      if( (frame/4) >= (spriteNum-2)){
+         frame = 0;
+      }
+   } if(mVelX >= 0){
+      if( (frame/4) >= spriteNum){
+         frame = 2*4;//4 is the delay number
+                     //if went back to 2 it would be on
+                     //sprite 0 actually
+      }
+   }
+}
